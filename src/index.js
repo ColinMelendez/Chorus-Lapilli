4f3154e0b9fca9c9863638be9cbec878b99ac484
Colin <cwrmcolin@gmail.com>
2021-10-27 20:13:44 -0700

    Core game completed. Set up the logic for taking turns and deciding on a winner for the game.

--- src/index.js
+++ src/index.js
@@ -2,31 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-      return (
-        <button 
-          className="square" 
-          onClick={() => this.props.onClick()}
-        >
-          {this.props.value}
-        </button>
-      );
-    }
+
+  function Square(props) {
+    return (
+      <button 
+        className="square" 
+        onClick={() => props.onClick()}
+      >
+        {props.value}
+      </button>
+    );
   }
   
   class Board extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        squares: Array(9).fill(null), 
+        squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
 
     handleClick(i) {
+      if (calculateWinner(this.state.squares) || this.state.squares[i]) {
+        return;
+      }
       const updatedSquares = this.state.squares.slice();
-      updatedSquares[i] = 'X';
-      this.setState({squares: updatedSquares});
+      updatedSquares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: updatedSquares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
 
     renderSquare(i) {
@@ -37,8 +43,14 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
-  
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
       return (
         <div>
           <div className="status">{status}</div>
@@ -84,4 +96,25 @@ class Square extends React.Component {
     <Game />,
     document.getElementById('root')
   );
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  
   
